aa2c0ab2fe1d555deb0b63feddbb4dc9d5f47fbf
AbraraliS <abrarali.sunasara28@gmail.com>
2025-02-27 08:48:06 +0530

    Updated

--- LinkedList/DLL.java
+++ LinkedList/DLL.java
@@ -2,24 +2,25 @@ package LinkedList;
 
 // import org.junit.platform.engine.support.hierarchical.Node;
 
-class Node {
-    int data;
-    Node next;
-    Node back;
 
-    Node(int data1, Node next1, Node back1) {
-        this.data = data1;
-        this.next = next1;
-        this.back = back1;
-    }
-
-    Node(int data1) {
-        this.data = data1;
-        this.next = null;
-        this.back = null;
-    }
-};
 public class DLL {
+   public static class Node {
+        int data;
+        Node next;
+        Node back;
+    
+        Node(int data1, Node next1, Node back1) {
+            this.data = data1;
+            this.next = next1;
+            this.back = back1;
+        }
+    
+        Node(int data1) {
+            this.data = data1;
+            this.next = null;
+            this.back = null;
+        }
+    };
     private static Node convert2All(int[] arr){
         if (arr == null || arr.length == 0) {
             return null;
@@ -44,6 +45,23 @@ public class DLL {
         System.out.println("NULL");
         return head;
     }
+    private static Node insertTail(Node head, int k) {
+        Node newNode = new Node(k);
+        if (head == null) {
+            return newNode;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next= newNode;
+        newNode.back = current;
+        return head;
+    }
+    private static Node insertHead(Node head, int k) {
+        return new Node(head, k)
+    }
+
     public static void main(String[] args) {
 
         int[] arr = { 4, 5, 6, 7, 8 };
@@ -51,6 +69,9 @@ public class DLL {
         // System.out.println(head.data);
         Node head = convert2All(arr);
         printNode(head);
-
+        head = insertTail(head, 100);
+        printNode(head);
+        head = insertHead(head, 200);
+        printNode(head);
     }
 }
